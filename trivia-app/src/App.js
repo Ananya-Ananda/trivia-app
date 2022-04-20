4b2740e3157af3abeeb98f5f6bd793f897efbd74
Ananya-Ananda <ananya.ananda201@gmail.com>
2022-04-20 15:22:03 -0400

    tittle

---
+++ trivia-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+//import React
+//import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { getByTitle } from '@testing-library/react';
+
+
+function App() {
+
+  // create questions
+  // display question title
+  //     question choices -> map the choices
+  //     one click -> disable clicking again + 
+  //     set result as correct or false
+  return (
+    // App title
+    <div className = "App-header" >
+      <h1>Trivia App:)</h1>
+      {/* map questions */}
+    </div> 
+
+  );
+}
+
+export default App;
